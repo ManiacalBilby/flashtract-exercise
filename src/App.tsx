bbbb5c4a1173121cc8c90d76534027553ad62231
Tyler McDonald <tyler@heliumservices.com>
2020-09-25 14:57:25 -0400

    Implement useRef hook to solve stale state issue in sendRequest function.

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import axios from "axios";
 import "./App.css";
 
 function App() {
   const [searchType, setSearchType] = useState("users");
   const [searchText, setSearchText] = useState();
+  const searchTypeRef = useRef();
+  //@ts-ignore
+  searchTypeRef.current = searchType;
 
   console.log("searchType", searchType);
   console.log("searchText", searchText);
   //@ts-ignore
-  function handleSearchTypeChange(e) {
-    setSearchType(e.target.value);
-
+  async function handleSearchTypeChange(e) {
+    await setSearchType(e.target.value);
     sendRequest();
   }
   //@ts-ignore
   function handleSearchTextChange(e) {
     //@ts-ignore
     setSearchText(`?q=${e.target.value}`);
+    sendRequest();
   }
-
-  const sendRequest = function () {
-    console.log("searchType in sendRequest", searchType);
+  //@ts-ignore
+  function sendRequest() {
+    console.log("searchType in sendRequest", searchTypeRef);
     //   axios
     //     .get(`https://api.github.com/search/${searchType}${searchText}`)
     //     .then(function (response) {
@@ -30,7 +33,7 @@ function App() {
     //     .catch(function (error) {
     //       console.log(error);
     //     });
-  };
+  }
   return (
     <div>
       <input
