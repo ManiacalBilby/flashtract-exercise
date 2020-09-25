fce39209a1272db81ede9852bf7f7b5795e18c80
Tyler McDonald <tyler@heliumservices.com>
2020-09-25 13:02:38 -0400

    Search is working. SearchType is being set inverse of intended currently.

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,50 @@ import axios from "axios";
 import "./App.css";
 
 function App() {
-  const [user, setUser] = useState("defunkt");
-  const [searchType, setSearchType] = useState("user");
+  const [searchType, setSearchType] = useState("users");
+  const [searchText, setSearchText] = useState();
+
+  console.log("searchType", searchType);
+  console.log("searchText", searchText);
+  //@ts-ignore
+  function handleSearchTypeChange(e) {
+    setSearchType(e.target.value);
+
+    sendRequest();
+  }
+  //@ts-ignore
+  function handleSearchTextChange(e) {
+    //@ts-ignore
+    setSearchText(`?q=${e.target.value}`);
+  }
+
   const sendRequest = function () {
-    axios
-      .get(`https://api.github.com/${searchType}/${user}`)
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+    console.log("searchType in sendRequest", searchType);
+    //   axios
+    //     .get(`https://api.github.com/search/${searchType}${searchText}`)
+    //     .then(function (response) {
+    //       console.log(response);
+    //     })
+    //     .catch(function (error) {
+    //       console.log(error);
+    //     });
   };
   return (
     <div>
-      <input placeholder="Start typing to search ..."></input>
+      <input
+        onChange={handleSearchTextChange}
+        placeholder="Start typing to search ..."
+      ></input>
+      <select
+        value={searchType}
+        //@ts-ignore
+        onChange={handleSearchTypeChange}
+        name="search-select"
+        id="search-select"
+      >
+        <option value="users">Users</option>
+        <option value="repositories">Repos</option>
+      </select>
     </div>
   );
 }
